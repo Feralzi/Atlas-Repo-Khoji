fbefe7b6db25d6bd11ab397bf27ed31b40ad93b0
feralzi <feralzi123@gmail.com>
2024-10-06 16:57:12 +0000

    File for BashScripts

---
+++ Bash-scripts/intro.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "This is my first script"
+echo "This is a bash script"
+echo
+echo "My PWD is:"
+pwd
+echo
+echo "Here are the contents of the PWD:"
+ls
+user=whoami
+echo
+echo "Welcome $user"
